f40ba58855c9ca1d5044b68fbb99b105dea4c492
hoho3419 <hoho3419@naver.com>
2022-12-03 16:44:26 +0900

    second commit

--- js/index.js
+++ js/index.js
@@ -16,16 +16,16 @@ $(document).ready(function(){
     // 리뷰 이미지 위치값
     const review_move = 460;
     const timer = 500;
-    // setInterval 변수 
+    // 메인,서브,리뷰  setInterval 변수 
     let main_interval;
+    let sub_interval;
+    let rev_interval;
+    $('.main_box').eq(main_idx).fadeIn(0)
+    // 자동으로 메인,서브,리뷰 슬라이드 작동하기
+    auto_main_slide();
+    auto_sub_slide();
+    auto_rev_slide()
     /************* 초기화 작업들 *************/
-    let rs = ITEM_LIST[0][0];
-    $('.main_img').eq(main_idx).css({display: "block"})
-    $('.top_desc').text(rs.desc)
-    $('.button_desc').text(rs.d_day)
-    // 배경색 동적으로 넣기
-    $('.first_section').css({background: rs.first_dack})
-    $('.second_section').css({background: rs.scecond_dack})
     // 메인 슬라이드에 아이콘 동적으로 넣기
     for(let i = 0;i<main_img_length;i++){
         let item = `<img class="indi" src="./img/main/item${i + 1}.jpg" alt="포스터">`;
@@ -47,8 +47,6 @@ $(document).ready(function(){
     $('.left_icon').on('click',function(){
         // 클릭하고 동적인 일들이 끝날때까지 버튼막기
         button_ban($('.left_icon'));
-        // 배열을 잡아줄때 -6보다 더 작아지거나 같으면 main_idx 초기화
-        if(main_idx <= -main_img_length){main_idx = 0}
         slide((main_idx - 1) % main_img_length,'left');
         main_idx--;
         // indi 클래스에 item1 다 삭제하고 현재 요소에 item1 클래스 넣어주기
@@ -59,28 +57,23 @@ $(document).ready(function(){
     $('.right_icon').on('click',function(){
         // 클릭하고 동적인 일들이 끝날때까지 버튼막기
         button_ban($('.right_icon'));
-        // 배열에 음수가 들어가지 못하게 -1이 되면 양수로 초기화 해주기
-        if(main_idx < 0){main_idx = main_idx*-1}
         slide((main_idx + 1) % main_img_length,'right');
         main_idx++;
         // indi 클래스에 item1 다 삭제하고 현재 요소에 item1 클래스 넣어주기 
         $('.indi').removeClass('item1');
         $('.indi').eq(main_idx % main_img_length).addClass('item1')
     })
+    // 메인배너 인디케이터 이벤트(클릭한 n번째 요소 배경,포스터,글자 바꾸기) 클릭한 인디케이터 상승
     $('.indi').on('click',function(){
-        // 현재 클릭한 인디케이터 순서 찾고 ITEM_LIST 에서 맞는 데이터 불러올 준비하기
+        // 현재 클릭한 요소가 현재 보여주고 있는 요소면 아무일도 일어나지 않음
         let curr_idx = $(this).index();
-        let item = ITEM_LIST[0][curr_idx];
-
-        // 클릭한 요소에 맞춰 데이터 보여주기
-        $('.main_img').delay(timer - 200).fadeOut(300);
-        $('.main_img').eq(curr_idx).fadeIn(timer);
-        $('.first_section').css({background: item.first_dack})
-        $('.second_section').css({background: item.scecond_dack})
-        $('.top_desc').text(item.desc)
-        $('.button_desc').text(item.d_day)
-        $('.indi').removeClass('item1');
-        $('.indi').eq(curr_idx).addClass('item1')
+        if(!$('.indi').eq(curr_idx).hasClass('item1')){
+            button_ban($('.indi'))
+            $('.main_box').fadeOut(timer)
+            $('.main_box').eq(curr_idx).fadeIn(timer)
+            $('.indi').removeClass('item1');
+            $('.indi').eq(curr_idx).addClass('item1')
+        }
     })
     // 서브 슬라이드 왼쪽 버튼 이벤트
     $('#sub_left').on('click',function(){
@@ -122,6 +115,7 @@ $(document).ready(function(){
         $('.review_img_box').animate({left: `+=${review_move}px`},timer)
         rev_dix--
     })
+    // 클릭하면 선택한 카테고리 랭킹 1,2,3위 보여주기
     $('.rank_item').on('click',function(){
         let curr = $(this).index();
         $('.rank_item').removeClass('rank_item_chk');
@@ -129,21 +123,37 @@ $(document).ready(function(){
         $('.rank_article').removeClass('dis_flex');
         $('.rank_article').eq(curr).addClass('dis_flex');
     })
+    // 마우스 호버시 해당 슬라이드 멈춤 떼면 다시 시작
+    $('.main_slide').hover(function(){
+        clearInterval(main_interval);
+    },function(){
+        auto_main_slide()
+    });
+    $('.sub_slide_section').hover(function(){
+        clearInterval(sub_interval);
+    },function(){
+        auto_sub_slide();
+    })
+    $('.review_area').hover(function(){
+        clearInterval(rev_interval);
+    },function(){
+        auto_rev_slide();
+    })
+    // 현재 선택된 인디케이터에 클래스 추가 함수
     function indi_chk(){
         $('.sub_indi_icon').removeClass('sub_icon_chk');
         $('.sub_indi_icon').eq(sub_idx % sub_img_length).addClass('sub_icon_chk');
     }
+    // 서브 슬라이드 함수
     function sub_slide(el,f_idx,f_move,s_dix,s_move){
         el.eq(f_idx).animate({left: f_move},timer)
         el.eq(s_dix).animate({left: s_move},0).animate({left: '0%'},timer)
     }
+    // 메인 슬라이드 함수
     function slide(s_idx,direction){
         // 지금 포스터에 맞는 ITEM_LIST 가르키기
-        let item = ITEM_LIST[0][(main_idx + 1) % main_img_length];
         if(direction == "left"){
-            // 배열 맨 뒤 잡아주기 -6이 되면 0으로 초기화 해서 맨뒤 요소 잡기
             // 맨 끝에 있는 인디케이터 위치 맞춰주고 들어오게 하고 모든 인디케이터 오른쪽으로 이동
-            item = ITEM_LIST[0][-1*((main_idx*-1) - 5) % main_img_length];
             $('.indi').eq((main_idx - 1) % main_img_length).animate({left: `${-main_img_move}`},0)
             $('.indi').animate({left: `+=${main_img_move}`},timer)
         }
@@ -151,31 +161,31 @@ $(document).ready(function(){
             // 모든 요소 먼저 왼쪽으로 이동하고 나갔던 맨 처음 인디케이터 맨 끝으로 이동시키기
             $('.indi').animate({left: `-=${main_img_move}`},timer).eq(main_idx % main_img_length).animate({left: 600},0)
         }
-        // 현재 보여줄 데이터 넣기
-        $('.main_img').eq(main_idx % main_img_length).delay(timer - 200).fadeOut(300);
-        $('.main_img').eq(s_idx).fadeIn(timer);
-        $('.first_section').css({background: item.first_dack})
-        $('.second_section').css({background: item.scecond_dack})
-        $('.top_desc').text(item.desc)
-        $('.button_desc').text(item.d_day)
+        // main_box 넣기
+        $('.main_box').fadeOut(timer)
+        $('.main_box').eq(s_idx).fadeIn(timer)
     }
+    // 버튼막는 함수
     function button_ban(el){
         el.css({pointerEvents: "none"})
         setTimeout(function(){
             el.css({pointerEvents: "auto"})
         },timer)
     }
+    // 메인,서브,리뷰 슬라이드 자동으로 실행
     function auto_main_slide(){
         main_interval = setInterval(function(){
             $('.right_icon').trigger('click');
-            $('#review_r_button').trigger('click')
+        },timer + 1800)
+    }
+    function auto_sub_slide(){
+        sub_interval = setInterval(function(){
             $('#sub_right').trigger('click')
         },timer + 1800)
     }
-    auto_main_slide();
-    $('.main_slide').hover(function(){
-        clearInterval(main_interval);
-    },function(){
-        auto_main_slide()
-    });
+    function auto_rev_slide(){
+        rev_interval = setInterval(function(){
+            $('#review_r_button').trigger('click')
+        },timer + 1800)
+    }
 })
